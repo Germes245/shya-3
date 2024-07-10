2800b78fa129f75167ed3ea95530a46db249f2aa
Germes245 <dan119@bk.ru>
2024-07-10 11:50:21 +0300

    create main.py and welcome message

---
+++ main.py
@@ -0,0 +1,15 @@
+from aiogram import Bot,Dispatcher,executor,types
+from configparser import ConfigParser
+config=ConfigParser()
+config.read(r'C:\python_projects\Aleksey\token.ini')
+token=config['Telegram']['shya_3']
+bot=Bot(token)
+dp=Dispatcher(bot)
+async def shya(_):
+    print('main')
+@dp.message_handler(commands=('start'))
+async def start(message:types.Message):
+    print(message)
+    await message.answer(text=f'hello {message.from_user.first_name}\nyour id is {message.from_user.id}')
+if __name__=='__main__':
+    executor.start_polling(dp,on_startup=shya)
